d31ad3d8285e844185f6c29fd8b8b5b60311037b
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-07 18:57:14 +0100

    added a Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import RootLayout from './layout'
+import RootHead from './head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <RootLayout>
+      <RootHead />
+      <Component {...pageProps} />
+    </RootLayout>
+  )
 }
