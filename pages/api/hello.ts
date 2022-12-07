f8870dda71c2c3293a897b7de2b93d27f5d08756
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-12-07 17:03:37 +0100

    Added test api data

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import db from  '../../database/test.json'
 
-type Data = {
-  name: string
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try{
+    const data = await testAsync()
+    res.status(200).json(data)
+  } catch(e){
+    res.status(500).json({ error: 'Operation failed: ' + e})
+  }
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export async function testAsync() {
+  return JSON.parse(JSON.stringify(db.person))
 }
