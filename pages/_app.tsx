d120b8c91c8c32285de2c219a0f9a447a5c62f98
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-08 10:26:19 +0100

    render after sessisonstorage is loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import RootLayout from './layout'
 import RootHead from './head'
-import React, { useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import Login from '../components/login'
+import useStorage from '../hooks/useStorage'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [login, setLogin] = useState(false)
+  const [render, setRender] = useState(false)
+  const { getItem } = useStorage()
+
+  useLayoutEffect(() => {
+    if (getItem('login') === 'true') {
+      setLogin(true)
+    }
+    setRender(true)
+  }, [])
 
   return (
+    render &&
     <>
       <RootHead />
       {login ? (
