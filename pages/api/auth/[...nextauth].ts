857d83a3248957fd36887576f3c324570cd5cd24
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-12-20 21:57:31 +0100

    Implemented salt to hash function

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,27 +1,54 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import  CredentialsProvider  from "next-auth/providers/credentials";
-import * as config from "../config.js"
 
-async function searchUser(email:string, pw:string){
+let maxAge = 15 * 60;
+const rememberMe = (remember: Boolean): void => {
+    maxAge = remember ? 30 * 24 * 60 * 60 : 15 * 60;
+}
+
+async function getSalt(email:string){
     const { MongoClient, ServerApiVersion } = require('mongodb');
-    const uri = "mongodb+srv://"+config.read_user+":"+config.read_password+"@passwordsafe.ownrlys.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://"+process.env.USERS_READ+":"+process.env.USERS_READ_PW+process.env.DB_URL+"/?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     try {
-      await client.connect();
-      const db = client.db("passwordsafe");
-      const collection = db.collection("users");
-      const query = { email: {$eq: email}, password: {$eq: pw}};
-      const options = {projection: { _id: 1, name: 1, email: 1, password: 1}};
-      const result = await collection.find(query, options).toArray();
-      return result[0];
-    } finally {
-      await client.close();
+        await client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection("users");
+        const query = { email: {$eq: email}};
+        const options = {projection: { _id: 0, salt: 1}};
+        const result = await collection.find(query, options).toArray();
+        return result[0].salt;
+    }finally {
+        await client.close();
     }
-  }
+}
+
+async function authUser(email:string){
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://"+process.env.USERS_READ+":"+process.env.USERS_READ_PW+process.env.DB_URL+"/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    try {
+        await client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const collection = db.collection("users");
+        const query = { email: {$eq: email}};
+        const options = {projection: { _id: 1, name: 1, email: 1, password: 1}};
+        const result = await collection.find(query, options).toArray();
+        return result[0];
+    } finally {
+        await client.close();
+    }
+}
+
+function hashPassword(target: string, salt:string): string {
+    const crypto = require('crypto');
+    return crypto.createHash('sha512').update(target+salt, 'utf-8').digest('hex');
+}
 
 const authOptions: NextAuthOptions = {
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt',
+        maxAge
     },
     providers: [
         CredentialsProvider({
@@ -31,15 +58,17 @@ const authOptions: NextAuthOptions = {
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials) {
-                let {email, password} = credentials as {email: string, password: string};
-                const fetch = await searchUser(email, password)
+                let {email, password, remember} = credentials as {email: string, password: string, remember: boolean};
+                const fetch = await authUser(email)
+                const salt = await getSalt(email);
                 const user = fetch as {id:string, name: string, email: string, password: string};
-                if (user.email === email && user.password === password) {
-                    return user
-                }
-                else{
-                    return null
-                }
+                if (user.email === email) {
+                    if(hashPassword(password,salt) === user.password){
+                        rememberMe(remember);
+                        return user
+                    }
+                    else{return null}
+                }else{return null}
             }
         })
     ],
