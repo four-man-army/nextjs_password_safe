c942f9d00a8b3d281c66152b9e2afcedae0fa6d4
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-12-20 18:35:01 +0100

    Removed test function

--- pages/safe.tsx
+++ pages/safe.tsx
@@ -3,23 +3,10 @@ import { Typography } from "antd";
 const { Title } = Typography;
 
 export default function Home() {
-  fetch("http://localhost:3000/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: "ferrer@mail.com",
-            password: "1234",
-            })
-        }).then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
+  console.log()
   return (
     <>
       <Title>Password Safe</Title>
-      
-      
     </>
   );
 }
