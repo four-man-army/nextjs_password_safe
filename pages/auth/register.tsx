25c306d5f53a129ad3d1879d31c3f9ea2e717811
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-21 11:10:41 +0100

    removed remember me button

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -89,14 +89,6 @@ const Register = (): JSX.Element => {
                 <Input.Password />
               </Form.Item>
 
-            <Form.Item
-              name="remember"
-              valuePropName="checked"
-              wrapperCol={{ offset: 8, span: 16 }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
             <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
               <Button type="primary" htmlType="submit">
                 Submit
