f2931dff879883bd1612556760ea832d0bd9364b
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-21 18:23:17 +0100

    Added password safe panel

--- pages/safe.tsx
+++ pages/safe.tsx
@@ -1,8 +1,9 @@
-import { Typography } from "antd";
+import { Button, Card, Col, Input, Row, Space, Tooltip, Typography } from "antd";
 import { useSession } from "next-auth/react";
-import Link from "next/link";
 import Router from "next/router";
 import { useEffect } from "react";
+import styles from "../styles/Safe.module.css";
+import { EyeOutlined } from "@ant-design/icons";
 
 const { Title } = Typography;
 
@@ -17,6 +18,29 @@ export default function Home() {
     return (
       <>
         <Title>Password Safe</Title>
+        <div className={styles.container}>
+          <Space align="center" className={styles.paper}>
+            <Card className={styles.card}>
+              <Row gutter={48}>
+                <Col span={8}>
+                  <Input value={"google.com"} />
+                </Col>
+                <Col span={8}>
+                  <Input value={"olliXD123"} />
+                </Col>
+                <Col span={8}>  
+                  <Input.Group compact>
+                    <Input.Password disabled value={"pppppp"} visibilityToggle={false} style={{width: "calc(100% - 32px)"}} />
+                    <Tooltip title="view password">
+                      <Button icon={<EyeOutlined />} />
+                    </Tooltip>
+                  </Input.Group>
+                </Col>
+              </Row>
+              <Button className={styles.button} type="primary">+ Add</Button>
+            </Card>
+          </Space>
+        </div>
       </>
     );
 
