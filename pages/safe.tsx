2f5685bd2e0adbc7271bead570a2f026b55405b9
Oliver Oswald <oliver.oswald25@gmail.com>
2022-12-23 11:52:43 +0100

    add passwords

--- pages/safe.tsx
+++ pages/safe.tsx
@@ -10,7 +10,7 @@ import {
 } from "antd";
 import { useSession } from "next-auth/react";
 import Router from "next/router";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Safe.module.css";
 import {
   EyeOutlined,
@@ -20,23 +20,18 @@ import {
 
 const { Title, Paragraph } = Typography;
 
-type ListItem = {
+interface ListItem {
   id: number;
   title: string;
   user: string;
   password: string;
-};
+}
 
 export default function Home() {
   const { status, data } = useSession();
-  const [list, setList] = useState<ListItem[]>([
-    {
-      id: 1,
-      title: "google.com",
-      user: "olliXD123",
-      password: "hjkHDusza7zHIHjIOu8",
-    },
-  ]);
+  const [list, setList] = useState<ListItem[]>();
+  const [listItem, setListItem] = useState<ListItem>();
+  const [adding, setAdding] = useState<boolean>(false);
 
   useEffect(() => {
     if (status === "unauthenticated") Router.replace("/auth/signin");
@@ -49,25 +44,95 @@ export default function Home() {
         <div className={styles.container}>
           <Space align="center" className={styles.paper}>
             <Card className={styles.card}>
-            {list && (
-              <Row className={styles.row} gutter={48}>
-                <Col span={8}>
-                  <Title level={3}>Website</Title>
-                </Col>
-                <Col span={8}>
-                  <Title level={3}>Username</Title>
-                </Col>
-                <Col span={8}>
-                  <Title className={styles.show} level={3}>Password</Title>
-                </Col>
-              </Row>
-            )}
-              {list?.map((listItem: ListItem) => (
-                <Node key={listItem.id} item={listItem} />
-              ))}
-              <Button className={styles.button} type="primary">
-                + Add
-              </Button>
+              <>
+                {list || adding && (
+                    <Row className={styles.row} gutter={48}>
+                      <Col span={8}>
+                        <Title level={3}>Website</Title>
+                      </Col>
+                      <Col span={8}>
+                        <Title level={3}>Username</Title>
+                      </Col>
+                      <Col span={8}>
+                        <Title className={styles.show} level={3}>
+                          Password
+                        </Title>
+                      </Col>
+                    </Row>
+                  )}
+                {list?.map((listItem: ListItem) => (
+                  <Node item={listItem} />
+                ))}
+                {adding && (
+                  <>
+                    <Row className={styles.row} gutter={48}>
+                      <Col span={8}>
+                        <Input
+                          onChange={(e) => {
+                            setListItem({
+                              ...listItem,
+                              title: e.target.value,
+                            } as ListItem);
+                          }}
+                        />
+                      </Col>
+                      <Col span={8}>
+                        <Input
+                          onChange={(e) => {
+                            setListItem({
+                              ...listItem,
+                              user: e.target.value,
+                            } as ListItem);
+                          }}
+                        />
+                      </Col>
+                      <Col span={8}>
+                        <Input
+                          onChange={(e) => {
+                            setListItem({
+                              ...listItem,
+                              password: e.target.value,
+                            } as ListItem);
+                          }}
+                        />
+                      </Col>
+                    </Row>
+                    <div className={styles.button}>
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          setAdding(false);
+                          if (list === undefined)
+                            setListItem({ ...listItem, id: 0 } as ListItem);
+                          else
+                            setListItem({
+                              ...listItem,
+                              id: list.length + 1,
+                            } as ListItem);
+                          if (listItem !== undefined) {
+                            if (list === undefined) setList([listItem]);
+                            else setList([...list, listItem]);
+                          }
+                        }}
+                      >
+                        Save
+                      </Button>
+                      <Button onClick={() => setAdding(false)}>Cancel</Button>
+                    </div>
+                  </>
+                )}
+                {!adding && (
+                  <Button
+                    className={list || adding ? styles.button : styles.add}
+                    onClick={() => {
+                      setAdding(true);
+                    }}
+                    type="primary"
+                  >
+                    + Add
+                  </Button>
+                )}
+              </>
             </Card>
           </Space>
         </div>
@@ -77,16 +142,16 @@ export default function Home() {
   return <div>loading...</div>;
 }
 
-const Node: any = (item: any): JSX.Element => {
+const Node: React.FC<{ item: ListItem }> = ({ item }) => {
   const [visible, setVisible] = useState<boolean>(false);
 
   return (
-    <Row className={styles.row} gutter={48}>
+    <Row className={styles.row} key={item.id} gutter={48}>
       <Col span={8}>
-        <Paragraph>{item.item.title}</Paragraph>
+        <Paragraph>{item.title}</Paragraph>
       </Col>
       <Col span={8}>
-        <Paragraph>{item.item.user}</Paragraph>
+        <Paragraph>{item.user}</Paragraph>
       </Col>
       <Col span={8}>
         <Paragraph
@@ -97,12 +162,13 @@ const Node: any = (item: any): JSX.Element => {
               : [<EyeOutlined key={1} />, <EyeOutlined key={2} />],
             onCopy: () => {
               setVisible(!visible);
-              if (visible) navigator.clipboard.writeText(item.password);
+              if (visible)
+                navigator.clipboard.writeText(item.password as string);
             },
             tooltips: visible ? ["Hide", "Copied!"] : ["Show", "Hidden"],
           }}
         >
-          {visible ? item.item.password : "muschi"}
+          {visible ? item.password : "muschi"}
         </Paragraph>
       </Col>
     </Row>
