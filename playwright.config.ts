ebb5b5c2e5235a45957eba7315af790b742cb851
Oliver Oswald <oliver.oswald25@gmail.com>
2023-01-02 21:16:20 +0100

    made .env file accessible

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+require('dotenv').config();
 
 /**
  * Read environment variables from file.
