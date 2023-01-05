3a9b9c662822393525e5703ad52645aa8f3856e1
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-05 21:27:56 +0100

    Prepared vault api (WIP)

--- pages/api/userSafe.ts
+++ pages/api/userSafe.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { JSEncrypt } from "jsencrypt";
 import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import Router from 'next/router';
 
 async function insertPassword(name: string, username: string, password: string, id: string) {
     const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -15,14 +13,13 @@ async function insertPassword(name: string, username: string, password: string, 
       //const db = client.db(process.env.DB_NAME);
       const db = client.db(process.env.TEST_DB); //! Using test db
       const collection = db.collection("users");
-      const query = { email: { $eq: email } };
+      const query = { email: { $eq: "" } };
       const options = { projection: { _id: 1 } };
       const result = await collection.find(query, options).toArray();
       if (result.length > 0) {
         return { error: 'Email already in use' }
       } else {
         return await db.collection('users').insertOne({
-          email: email,
           name: name,
           //password: hashPassword(pw, salt),
           salt: salt
@@ -33,35 +30,30 @@ async function insertPassword(name: string, username: string, password: string, 
     }
   }
 
+  async function checkAuth() {
+    const { status, data } = useSession()
+    if (status === "unauthenticated") return false
+    return true
+  }
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
 
-  const { email } = req.body
-  const { name } = req.body
-  const { password } = req.body
+  const { status } = req.body
+  const { session } = req.body
 
-  const { status, data } = useSession()
-
-  useEffect(() => {
-    if (status === "unauthenticated") res.status(401).json({ error: 'Not authenticated' })
-  }, [status])
-
-  if (req.method === 'POST') {
+  if (status === "unauthenticated") res.status(401).json({ error: 'Not authenticated' })
+  else{
+    if (req.method === 'POST') {
     try {
-      const data = await registerUser(email, name, password)
-      if (data.error) {
-        res.status(409).json({ error: data.error });
-      } else {
-        const userID = data.insertedId;
-        res.status(200).json(data.acknowledged)
-      }
+        res.status(200).json("200")
     } catch (e) {
       res.status(500).json({ error: 'Operation failed' })
     }
   } else {
     res.status(405).json({ error: 'Only POST is supported' })
   }
-
+  }
 }
