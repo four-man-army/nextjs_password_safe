75789baec83d4a86305f76202dcf40de1032d4d3
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-05 07:55:27 +0100

    Improved login and register frontend

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,43 +1,40 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import  CredentialsProvider  from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import * as argon2 from "argon2";
+
+const uri = "mongodb+srv://" + process.env.USERS_READ + ":" + process.env.USERS_READ_PW + process.env.DB_URL + "/?retryWrites=true&w=majority";
 
 let maxAge = 15 * 60;
 const rememberMe = (remember: Boolean): void => {
     maxAge = remember ? 30 * 24 * 60 * 60 : 15 * 60;
 }
 
-async function getSalt(email:string){
+async function getSalt(email: string) {
     const { MongoClient, ServerApiVersion } = require('mongodb');
-    //const uri = "mongodb+srv://"+process.env.USERS_WRITE+":"+process.env.USERS_WRITE_PW+process.env.DB_URL+"/?retryWrites=true&w=majority";
-    const uri = "mongodb+srv://" + process.env.TEST_USER + ":" + process.env.TEST_USER_PW + process.env.DB_URL + "/?retryWrites=true&w=majority"; //! Using test db 
+
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     try {
         await client.connect();
-        //const db = client.db(process.env.DB_NAME);
-        const db = client.db(process.env.TEST_DB); //! Using test db
+        const db = client.db(process.env.DB_NAME);
         const collection = db.collection("users");
-        const query = { email: {$eq: email}};
-        const options = {projection: { _id: 0, salt: 1}};
+        const query = { email: { $eq: email } };
+        const options = { projection: { _id: 0, salt: 1 } };
         const result = await collection.find(query, options).toArray();
         return result[0].salt;
-    }finally {
+    } finally {
         await client.close();
     }
 }
 
-async function authUser(email:string){
+async function authUser(email: string) {
     const { MongoClient, ServerApiVersion } = require('mongodb');
-    //const uri = "mongodb+srv://"+process.env.USERS_WRITE+":"+process.env.USERS_WRITE_PW+process.env.DB_URL+"/?retryWrites=true&w=majority";
-    const uri = "mongodb+srv://" + process.env.TEST_USER + ":" + process.env.TEST_USER_PW + process.env.DB_URL + "/?retryWrites=true&w=majority"; //! Using test db 
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     try {
         await client.connect();
-        //const db = client.db(process.env.DB_NAME);
-        const db = client.db(process.env.TEST_DB); //! Using test db
+        const db = client.db(process.env.DB_NAME);
         const collection = db.collection("users");
-        const query = { email: {$eq: email}};
-        const options = {projection: { _id: 1, name: 1, email: 1, password: 1}};
+        const query = { email: { $eq: email } };
+        const options = { projection: { _id: 1, name: 1, email: 1, password: 1 } };
         const result = await collection.find(query, options).toArray();
         return result[0];
     } finally {
@@ -45,9 +42,8 @@ async function authUser(email:string){
     }
 }
 
-async function hashPassword(target: string, salt:string): Promise<string> {
-    
-    return await argon2.hash(target, {type: argon2.argon2i, hashLength:32, salt: Buffer.from(salt, 'hex')})
+async function hashPassword(target: string, salt: string): Promise<string> {
+    return await argon2.hash(target, { type: argon2.argon2i, hashLength: 32, salt: Buffer.from(salt, 'hex') })
 }
 
 const authOptions: NextAuthOptions = {
@@ -63,17 +59,17 @@ const authOptions: NextAuthOptions = {
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials) {
-                let {email, password, remember} = credentials as {email: string, password: string, remember: boolean};
+                let { email, password, remember } = credentials as { email: string, password: string, remember: boolean };
                 const fetch = await authUser(email)
                 const salt = await getSalt(email);
-                const user = fetch as {id:string, name: string, email: string, password: string};
+                const user = fetch as { id: string, name: string, email: string, password: string };
                 if (user.email === email) {
-                    if(await hashPassword(password, salt) === user.password){
+                    if (await hashPassword(password, salt) === user.password) {
                         rememberMe(remember);
                         return user
                     }
-                    else{return null}
-                }else{return null}
+                    else { return null }
+                } else { return null }
             }
         })
     ],
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,64 +1,66 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import * as argon2 from "argon2";
 
-async function hashPassword(target: string, salt:string): Promise<string> {
-    return await argon2.hash(target, {type: argon2.argon2i, hashLength:32, salt: Buffer.from(salt, 'hex')})
+async function hashPassword(target: string, salt: string): Promise<string> {
+  return await argon2.hash(target, { type: argon2.argon2i, hashLength: 32, salt: Buffer.from(salt, 'hex') })
 }
 
 function generateSalt(): string {
-    const crypto = require('crypto');
-    return crypto.randomBytes(16).toString('hex');
+  const crypto = require('crypto');
+  return crypto.randomBytes(16).toString('hex');
 }
 
-async function registerUser(email:string, name:string, pw:string){
-    const { MongoClient, ServerApiVersion } = require('mongodb');
-    //const uri = "mongodb+srv://"+process.env.USERS_WRITE+":"+process.env.USERS_WRITE_PW+process.env.DB_URL+"/?retryWrites=true&w=majority";
-    const uri = "mongodb+srv://" + process.env.TEST_USER + ":" + process.env.TEST_USER_PW + process.env.DB_URL + "/?retryWrites=true&w=majority"; //! Using test db 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-    const salt = generateSalt();
-    try {
-      await client.connect();
-      //const db = client.db(process.env.DB_NAME);
-      const db = client.db(process.env.TEST_DB); //! Using test db
-      const collection = db.collection("users");
-      const query = { email: {$eq: email}};
-      const options = {projection: { _id: 1 }};
-      const result = await collection.find(query, options).toArray();
-      if(result.length > 0){
-        return {error : 'Email already in use'}
-      }else{
-        return await db.collection('users').insertOne({
-          email: email,
-          name: name,
-          password: await hashPassword(pw, salt),
-          salt: salt
-        });
-      }
-    } finally {
-      await client.close();
+async function registerUser(email: string, name: string, pw: string) {
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://" + process.env.USERS_WRITE + ":" + process.env.USERS_WRITE_PW + process.env.DB_URL + "/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  const salt = generateSalt();
+  try {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME);
+    const collection = db.collection("users");
+    const query = { email: { $eq: email } };
+    const options = { projection: { _id: 1 } };
+    const result = await collection.find(query, options).toArray();
+    if (result.length > 0) {
+      return { error: 'Email already in use' }
+    } else {
+      return await db.collection('users').insertOne({
+        email: email,
+        name: name,
+        password: await hashPassword(pw, salt),
+        salt: salt
+      });
     }
+  } finally {
+    await client.close();
   }
+}
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-      const { email } = req.body
-      const { name } = req.body
-      const { password } = req.body
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email } = req.body
+  const { name } = req.body
+  const { password } = req.body
 
-      if (req.method === 'POST') {
-        try{ 
-            const data = await registerUser(email, name, password)
-            if(data.error) {
-              res.status(409).json({ error: data.error});
-            }else{
-              res.status(200).json(data.acknowledged)
-            }
-        } catch(e){
-          res.status(500).json({ error: 'Operation failed'})
-        }
+  if (req.method === 'POST') {
+    try {
+      const data = await registerUser(email, name, password)
+      if (data.error) {
+        res.status(409).json({ error: data.error });
       } else {
-          res.status(405).json({ error: 'Only POST is supported'})
-        }
+        res.status(200).json(data.acknowledged)
+      }
+    } catch (e: any) {
+      if (e === 'Email already in use') {
+        res.status(500).json({ error: e })
+      } else {
+        res.status(500).json({ error: 'Something went wrong' })
+      }
+    }
+  } else {
+    res.status(405).json({ error: 'Only POST is supported' })
   }
+}
