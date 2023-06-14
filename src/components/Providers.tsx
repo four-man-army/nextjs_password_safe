bd3d0619ee90eec7854f36966a08086384f0ae95
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 21:55:47 +0200

    added query client

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,21 @@
 import { NavbarOpenProvider } from "@/context/NavbarOpen";
 import { PasswordProvider } from "@/context/Password";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
+  const query = new QueryClient();
   return (
     <PasswordProvider>
-      <NavbarOpenProvider>{children}</NavbarOpenProvider>
+      <Toaster position="top-center" reverseOrder={false} />
+      <QueryClientProvider client={query}>
+        <NavbarOpenProvider>{children}</NavbarOpenProvider>
+      </QueryClientProvider>
     </PasswordProvider>
   );
 };
