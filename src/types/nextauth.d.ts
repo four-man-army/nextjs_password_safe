6c11ab545eb4f8e70cc74842c28eea332616f701
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 10:29:51 +0200

    aded types

--- src/types/nextauth.d.ts
+++ src/types/nextauth.d.ts
@@ -3,7 +3,7 @@ import type { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
-    user: User & { id: string };
+    user: User & { id: string; encryptKey: string };
   }
 }
 
