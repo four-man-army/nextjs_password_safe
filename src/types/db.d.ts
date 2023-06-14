2927980edb6f92e152a3e081e24d314e2c9b8533
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 09:15:00 +0200

    added types

---
+++ src/types/db.d.ts
@@ -0,0 +1,6 @@
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  image: string;
+}
---
+++ src/types/nextauth.d.ts
@@ -0,0 +1,14 @@
+import type { Session, User } from "next-auth";
+import type { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: User & { id: string };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
