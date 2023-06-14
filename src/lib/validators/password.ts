5be305b935e951c7567b4d3a6cb7cc12e2c043db
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 12:49:52 +0200

    added password validator

---
+++ src/lib/validators/password.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const passwordValidator = z.object({
+    id: z.string(),
+    website: z.string(),
+    username: z.string(),
+    password: z.string(),
+})
+
+export const passwordListValidator = z.array(passwordValidator);
+
+export type Password = z.infer<typeof passwordValidator>;
+
+export type PasswordList = z.infer<typeof passwordListValidator>;
+
