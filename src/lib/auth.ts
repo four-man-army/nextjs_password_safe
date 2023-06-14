83ff3dbf94cc5d821c146bba075a5d82b7b1db8e
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 09:14:03 +0200

    added next auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,59 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "./db";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+
+function getGoogleCredentials() {
+  const googleClientId = process.env.GOOGLE_CLIENT_ID;
+  const googleKey = process.env.GOOGLE_KEY;
+
+  if (!googleClientId || !googleKey) {
+    throw new Error("Missing Google credentials");
+  }
+
+  return {
+    clientId: googleClientId,
+    clientSecret: googleKey,
+  };
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [GoogleProvider(getGoogleCredentials())],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        email: dbUser.email,
+        name: dbUser.name,
+        picture: dbUser.image,
+      };
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.image = token.picture;
+      }
+      return session;
+    },
+    redirect() {
+      return "/";
+    },
+  },
+};
