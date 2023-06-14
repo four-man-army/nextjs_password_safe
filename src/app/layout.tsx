4f084c13a2ecba027873dda031f83265d303f9de
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-14 10:05:58 +0200

    added login button to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,24 @@ import Navbar from "@/components/Navbar";
 import NavbarToggle from "@/components/NavbarToggle";
 import { Metadata } from "next";
 import "./globals.css"
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import Image from "next/image";
+import SignOutButton from "@/components/SignOutButton";
 
 export const metadata: Metadata = {
-  title: "Password Safe | Home",
+  title: "Password Safe",
   description: "Home page",
-}
+};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: ReactNode;
-}): JSX.Element {
+  }) {
+  
+  const session = await getServerSession(authOptions);
+  
   return (
     <html lang="en">
       <Providers>
@@ -24,8 +31,47 @@ export default function RootLayout({
             <section className="flex flex-row h-full">
               <Navbar />
               <section className="flex flex-col w-full bg-slate-100">
-                <header className="h-16 bg-white">
+                <header className="h-16 bg-white flex flex-row">
                   <NavbarToggle />
+                  <div className="ml-auto p-2 flex flex-row">
+                    {!session?.user ? (
+                      <div className="my-auto mr-4">
+                        <a
+                          href="/login"
+                          className="bg-blue-950 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300"
+                        >                 
+                          Login
+                        </a>
+                      </div>
+                    ) : (
+                      <div className="mx-6 mt-auto flex items-center">
+                        <div className="flex flex-1 items-center gap-x-4 text-sm font-semibold leading-6 text-gray-900">
+                          <div className="relative h-8 w-8">
+                            <Image
+                              fill
+                              referrerPolicy="no-referrer"
+                              className="rounded-full"
+                              src={session.user.image || ""}
+                              alt="Your profile picture"
+                            />
+                          </div>
+
+                          <span className="sr-only">Your profile</span>
+                          <div className="flex flex-col">
+                            <span aria-hidden="true">{session.user.name}</span>
+                            <span
+                              className="text-xs text-gray-500"
+                              aria-hidden="true"
+                            >
+                              {session.user.email}
+                            </span>
+                          </div>
+                        </div>
+
+                        <SignOutButton className="h-full aspect-square" />
+                      </div>
+                    )}
+                  </div>
                 </header>
                 <main className="my-6 mx-4 p-6 h-full bg-white">
                   {children}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Button from "@/components/ui/Button";
-import { FC, useState } from "react";
+import { useState } from "react";
 import { signIn } from "next-auth/react";
 import toast from "react-hot-toast";
+import Image from "next/image";
 
-const Page: FC = () => {
+export default function Page() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   async function loginWithGoogle() {
@@ -24,7 +25,9 @@ const Page: FC = () => {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            <div className="w-20 h-20 relative rounded-full">
+            <Image src="/logo.jpg" fill alt="logo" className="rounded-full" />
+            </div>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
@@ -73,5 +76,3 @@ const Page: FC = () => {
     </>
   );
 };
-
-export default Page;
