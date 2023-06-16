d8c6421307e112e68df9257f4650548704a4af15
Oliver Oswald <oliver.oswald25@gmail.com>
2023-06-16 15:48:36 +0200

    added genpwd function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,24 @@ export function encrypt(password: Password, key: string) {
 export function decrypt(text: string, key: string) {
   return AES.decrypt((JSON.parse(text) as DBMember).ct, key).toString(CryptoJS.enc.Utf8);
 }
+
+export function genPw(length: number, specialChars: boolean): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const specialCharacters = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+  var result = "";
+
+  if (specialChars) {
+    for (var i = 0; i < length; i++) {
+      var list = characters + specialCharacters;
+      result += list.charAt(Math.random() * list.length);
+    }
+    return result;
+  } else {
+    var list = characters;
+    for (var i = 0; i < length; i++) {
+      result += list.charAt(Math.random() * list.length);
+    }
+    return result;
+  }
+}
