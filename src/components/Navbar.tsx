4064b4b77d719534eb0eb0dbbbcae5b3e69c4624
Oliver Oswald <oliver.oswald@zuhlke.com>
2023-09-19 11:31:41 +0200

    made navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,95 +11,115 @@ import { cn } from "@/lib/utils";
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
-  const { navbarOpen } = useContext(NavbarOpenContext);
+  const { navbarOpen, setNavbarOpen } = useContext(NavbarOpenContext);
   const path = usePathname();
+  let width = window.innerWidth;
   return (
-    <aside
-      className={cn("bg-blue-950 transition-all duration-300 sm:w-20 w-0 fixed sm:relative z-10 h-full", {
-        "w-52 sm:w-52": navbarOpen,
-      })}
-    >
-      <div className="flex h-6 m-4 bg-red-500 justify-center relative">
-        <Image src="/logo.jpg" alt="logo" width={24} height={24} />
-      </div>
-      <ul className="w-full text-slate-300 text-sm">
-        <li className="h-full w-full">
-          <Link
-            href="/"
-            className={cn(
-              "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
-              {
-                "bg-blue-500": path === "/",
-              }
-            )}
-          >
-            <span className="flex">
-              <Home className="w-4 my-auto" />
-            </span>
-            <span
+    <>
+      {width < 640 && (
+        <div
+          onClick={() => setNavbarOpen(false)}
+          className={cn(
+            "absolute z-40 top-0 left-0 w-screen h-screen transition-all duration-300 backdrop-blur-sm",
+            {
+              hidden: !navbarOpen,
+            }
+          )}
+        />
+      )}
+      <aside
+        className={cn(
+          "bg-blue-950 transition-all z-50 duration-300 sm:w-20 w-0 fixed sm:relative h-full",
+          {
+            "w-52 sm:w-52": navbarOpen,
+          }
+        )}
+      >
+        <div className="flex h-6 m-4 bg-red-500 justify-center relative">
+          <Image src="/logo.jpg" alt="logo" width={24} height={24} />
+        </div>
+        <ul className={cn("w-full text-slate-300 text-sm overflow-hidden")}>
+          <li className="h-full w-full">
+            <Link
+              href="/"
               className={cn(
-                "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
                 {
-                  "w-full": navbarOpen,
+                  "bg-blue-500": path === "/",
                 }
               )}
+              onClick={() => width < 640 && setNavbarOpen(false)}
             >
-              Home
-            </span>
-          </Link>
-        </li>
-        <li className="h-full w-full">
-          <Link
-            href="/safe"
-            className={cn(
-              "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
-              {
-                "bg-blue-500": path === "/safe",
-              }
-            )}
-          >
-            <span className="flex">
-              <Key className="w-4 my-auto" />
-            </span>
-            <span
+              <span className="flex">
+                <Home className="w-4 my-auto" />
+              </span>
+              <span
+                className={cn(
+                  "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                  {
+                    "w-full": navbarOpen,
+                  }
+                )}
+              >
+                Home
+              </span>
+            </Link>
+          </li>
+          <li className="h-full w-full">
+            <Link
+              href="/safe"
               className={cn(
-                "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
                 {
-                  "w-full": navbarOpen,
+                  "bg-blue-500": path === "/safe",
                 }
               )}
+              onClick={() => width < 640 && setNavbarOpen(false)}
             >
-              Password Safe
-            </span>
-          </Link>
-        </li>
-        <li className="h-full w-full">
-          <Link
-            href="/generate"
-            className={cn(
-              "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
-              {
-                "bg-blue-500": path === "/generate",
-              }
-            )}
-          >
-            <span className="flex">
-              <Bot className="w-4 my-auto" />
-            </span>
-            <span
+              <span className="flex">
+                <Key className="w-4 my-auto" />
+              </span>
+              <span
+                className={cn(
+                  "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                  {
+                    "w-full": navbarOpen,
+                  }
+                )}
+              >
+                Password Safe
+              </span>
+            </Link>
+          </li>
+          <li className="h-full w-full">
+            <Link
+              href="/generate"
               className={cn(
-                "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                "flex m-1 px-6 gap-2 justify-end h-10 rounded-lg hover:text-white transition-all bg-transparent",
                 {
-                  "w-full": navbarOpen,
+                  "bg-blue-500": path === "/generate",
                 }
               )}
+              onClick={() => width < 640 && setNavbarOpen(false)}
             >
-              Password Genrator
-            </span>
-          </Link>
-        </li>
-      </ul>
-    </aside>
+              <span className="flex">
+                <Bot className="w-4 my-auto" />
+              </span>
+              <span
+                className={cn(
+                  "overflow-hidden truncate transition-all duration-300 h-fit my-auto w-0 flex-shrink",
+                  {
+                    "w-full": navbarOpen,
+                  }
+                )}
+              >
+                Password Genrator
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </aside>
+    </>
   );
 };
 
