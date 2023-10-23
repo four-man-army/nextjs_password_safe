76df2157601cac172bc5296f3bdb8622af10441a
Oliver Oswald <oliver.oswald25@gmail.com>
2023-10-23 15:44:09 +0200

    added password input animation

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -2,25 +2,16 @@
 import { trpc } from "@/app/_trpc/client";
 import { PasswordContext } from "@/context/Password";
 import { Password, passwordValidator } from "@/lib/validators/password";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 import { nanoid } from "nanoid";
-import { FC, FormEvent, useContext, useState } from "react";
+import { FC, useContext, useState } from "react";
+import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import Button from "./ui/Button";
 import { Input } from "./ui/Input";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 interface PasswordInputProps {}
-
-interface FormElements extends HTMLFormControlsCollection {
-  website: HTMLInputElement;
-  username: HTMLInputElement;
-  password: HTMLInputElement;
-}
-
-interface PasswordForm extends HTMLFormElement {
-  readonly elements: FormElements;
-}
 
 const PasswordInput: FC<PasswordInputProps> = ({}) => {
   const { addPassword: add, removePassword } = useContext(PasswordContext);
@@ -30,13 +21,13 @@ const PasswordInput: FC<PasswordInputProps> = ({}) => {
     add(password);
     setIsAdding(false);
     mutate(password);
-  }
+  };
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm<Password>({
     defaultValues: {
       id: nanoid(),
@@ -54,61 +45,97 @@ const PasswordInput: FC<PasswordInputProps> = ({}) => {
     },
   });
 
+  const input: Variants = {
+    show: { y: 0, opacity: 1 },
+    hidden: { y: -100, opacity: 0 },
+    close: { opacity: 0 },
+  };
+
+  const closeButton: Variants = {
+    show: { width: "100%" },
+    hidden: { width: 0 },
+    close: { width: 0, padding: 0 },
+  };
+
   const [isAdding, setIsAdding] = useState(false);
-  if (isAdding)
-    return (
-      <div className="w-full mt-4">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-row gap-2">
-            <Input
-              {...register("username")}
-              autoFocus
-              type="text"
-              placeholder="Username"
-              id="username"
-              error={errors.username?.message}
-            />
-            <Input
-              {...register("website")}
-              type="text"
-              placeholder="Website"
-              id="website"
-              error={errors.website?.message}
-            />
-            <Input
-              {...register("password")}
-              type="text"
-              placeholder="Password"
-              id="password"
-              error={errors.password?.message}
-            />
-          </div>
-          <div className="flex flex-row gap-4 mt-4 w-full">
-            <Button
-              type="reset"
-              variant="error"
-              onClick={() => setIsAdding(false)}
-              className="w-full"
-            >
-              Cancel
-            </Button>
-            <Button isLoading={isLoading} type="submit" className="w-full">
-              Add
-            </Button>
-          </div>
-        </form>
-      </div>
-    );
   return (
-    <div className="w-full mt-4">
-      <Button
-        onClick={() => setIsAdding(true)}
-        isLoading={isLoading}
-        className="w-full"
-      >
-        Add
-      </Button>
-    </div>
+    <AnimatePresence mode="wait">
+      {isAdding ? (
+        <div className="w-full mt-4" key="open">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <motion.div
+              key="expand"
+              variants={input}
+              animate="show"
+              initial="hidden"
+              exit="close"
+              className="flex flex-row gap-2 justify-between"
+              >
+              <Input
+                {...register("username")}
+                autoFocus
+                type="text"
+                placeholder="Username"
+                id="username"
+                error={errors.username?.message}
+              />
+              <Input
+                {...register("website")}
+                type="text"
+                placeholder="Website"
+                id="website"
+                error={errors.website?.message}
+                />
+              <Input
+                {...register("password")}
+                type="text"
+                placeholder="Password"
+                id="password"
+                error={errors.password?.message}
+              />
+            </motion.div>
+            <motion.div className="flex flex-row gap-4 mt-4 w-full" exit={{gap:0}}>
+              <motion.div
+                key="close"
+                variants={closeButton}
+                animate="show"
+                initial="hidden"
+                exit="close"
+                className="w-full overflow-hidden"
+              >
+                <Button
+                  type="reset"
+                  variant="error"
+                  onClick={() => setIsAdding(false)}
+                  className="w-full"
+                  onFocus={(e) => e.currentTarget.blur()}
+                >
+                  <motion.p exit={{x: -100}}>
+                  Cancel
+                  </motion.p>
+                </Button>
+              </motion.div>
+              <motion.div className="w-full" layoutId="1">
+                <Button type="submit" className="w-full">
+                  <motion.p layoutId="2">Add</motion.p>
+                </Button>
+              </motion.div>
+            </motion.div>
+          </form>
+        </div>
+      ) : (
+        <motion.div layoutId="1" className="w-full mt-4" key="closed">
+          <Button
+            type="submit"
+            onClick={() => setIsAdding(true)}
+            isLoading={isLoading}
+            className="w-full"
+            >
+            <motion.p layoutId="2">Add</motion.p>
+          </Button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
