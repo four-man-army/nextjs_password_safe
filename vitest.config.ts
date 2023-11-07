e5f521ea8ffa2dd6230ff0815aa97da7f183fa56
Oliver Oswald <oliver.oswald25@gmail.com>
2023-11-07 10:21:55 +0100

    Add new dependencies and update configuration files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/unit/**/*.test.ts"],
+    reporters: ["html"],
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
