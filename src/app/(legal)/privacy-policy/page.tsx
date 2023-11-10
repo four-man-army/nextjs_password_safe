345546f314a40877ca84f856668204dfab264456
Oliver Oswald <oliver.oswald25@gmail.com>
2023-11-10 17:15:35 +0100

    fix eslint

--- src/app/(legal)/privacy-policy/page.tsx
+++ src/app/(legal)/privacy-policy/page.tsx
@@ -75,13 +75,13 @@ export default function Home() {
       <p>
         Password Safe follows a standard procedure of using log files. These
         files log visitors when they visit websites. All hosting companies do
-        this and a part of hosting services' analytics. The information
+        this and a part of hosting services&apos; analytics. The information
         collected by log files include internet protocol (IP) addresses, browser
         type, Internet Service Provider (ISP), date and time stamp,
         referring/exit pages, and possibly the number of clicks. These are not
         linked to any information that is personally identifiable. The purpose
         of the information is for analyzing trends, administering the site,
-        tracking users' movement on the website, and gathering demographic
+        tracking users&apos; movement on the website, and gathering demographic
         information.
       </p>
 
@@ -89,10 +89,10 @@ export default function Home() {
 
       <p>
         Like any other website, Password Safe uses "cookies". These cookies are
-        used to store information including visitors' preferences, and the pages
+        used to store information including visitors&apos; preferences, and the pages
         on the website that the visitor accessed or visited. The information is
-        used to optimize the users' experience by customizing our web page
-        content based on visitors' browser type and/or other information.
+        used to optimize the users&apos; experience by customizing our web page
+        content based on visitors&apos; browser type and/or other information.
       </p>
 
       <h2 className="text-md font-bold">
@@ -108,7 +108,7 @@ export default function Home() {
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Web Beacons that are used in their respective
         advertisements and links that appear on Password Safe, which are sent
-        directly to users' browser. They automatically receive your IP address
+        directly to users&apos; browser. They automatically receive your IP address
         when this occurs. These technologies are used to measure the
         effectiveness of their advertising campaigns and/or to personalize the
         advertising content that you see on websites that you visit.
@@ -122,7 +122,7 @@ export default function Home() {
       <h2 className="text-md font-bold">Third Party Privacy Policies</h2>
 
       <p>
-        Password Safe's Privacy Policy does not apply to other advertisers or
+        Password Safe&apos;s Privacy Policy does not apply to other advertisers or
         websites. Thus, we are advising you to consult the respective Privacy
         Policies of these third-party ad servers for more detailed information.
         It may include their practices and instructions about how to opt-out of
@@ -132,7 +132,7 @@ export default function Home() {
       <p>
         You can choose to disable cookies through your individual browser
         options. To know more detailed information about cookie management with
-        specific web browsers, it can be found at the browsers' respective
+        specific web browsers, it can be found at the browsers&apos; respective
         websites.
       </p>
 
@@ -145,7 +145,7 @@ export default function Home() {
         to:
       </p>
       <p>
-        Request that a business that collects a consumer's personal data
+        Request that a business that collects a consumer&apos;s personal data
         disclose the categories and specific pieces of personal data that a
         business has collected about consumers.
       </p>
@@ -154,8 +154,8 @@ export default function Home() {
         a business has collected.
       </p>
       <p>
-        Request that a business that sells a consumer's personal data, not sell
-        the consumer's personal data.
+        Request that a business that sells a consumer&apos;s personal data, not sell
+        the consumer&apos;s personal data.
       </p>
       <p>
         If you make a request, we have one month to respond to you. If you would
@@ -200,7 +200,7 @@ export default function Home() {
         like to exercise any of these rights, please contact us.
       </p>
 
-      <h2 className="text-md font-bold">Children's Information</h2>
+      <h2 className="text-md font-bold">Children&apos;s Information</h2>
 
       <p>
         Another part of our priority is adding protection for children while
