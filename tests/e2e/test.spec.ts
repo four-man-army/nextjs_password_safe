79622d5a1f2c9ee874aa20361dbc9b016e13b09e
Oliver Oswald <oliver.oswald25@gmail.com>
2023-11-13 13:01:45 +0100

    add e2e test for checkbox

--- tests/e2e/test.spec.ts
+++ tests/e2e/test.spec.ts
@@ -1,9 +1,27 @@
 import test, { expect } from "@playwright/test";
 
-test("should generate a password", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("link", { name: "Password Genrator" }).click();
+test("should test checkbox", async ({ page }) => {
+  await page.goto("/generate");
+  expect(page.getByLabel("Uppercase")).toBeChecked();
+  expect(page.getByLabel("Lowercase")).toBeChecked();
+  expect(page.getByLabel("Numbers")).toBeChecked();
+  expect(page.getByLabel("Symbols")).toBeChecked();
   await page.getByRole("radio").first().click();
+  expect(page.getByLabel("Uppercase")).toBeChecked();
+  expect(page.getByLabel("Lowercase")).toBeChecked();
+  expect(page.getByLabel("Numbers")).not.toBeChecked();
+  expect(page.getByLabel("Symbols")).not.toBeChecked();
+  expect(page.getByLabel("Numbers")).toBeDisabled();
+  expect(page.getByLabel("Symbols")).toBeDisabled();
   await page.getByRole("radio").nth(1).click();
+  expect(page.getByLabel("Uppercase")).toBeChecked();
+  expect(page.getByLabel("Lowercase")).toBeChecked();
+  expect(page.getByLabel("Numbers")).not.toBeChecked();
+  expect(page.getByLabel("Symbols")).not.toBeChecked();
   await page.getByRole("radio").nth(2).click();
+  expect(page.getByLabel("Uppercase")).toBeChecked();
+  expect(page.getByLabel("Lowercase")).toBeChecked();
+  expect(page.getByLabel("Numbers")).toBeChecked();
+  expect(page.getByLabel("Symbols")).toBeChecked();
+  await page.getByRole("radio").first().click();
 });
